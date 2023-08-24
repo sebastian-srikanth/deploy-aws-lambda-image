48263f3f941ecc6e06ea17d77379ae829f86004a
Sebastian Srikanth Kumar Selvaraj <sebastiansrikanth@Sebastians-Mac-mini.local>
2023-08-24 16:43:17 +0530

    trigger flow to test terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,11 @@
 variable "aws_region" {
     type = string
-    default = "ap-south-1"
 }
 
 variable "backend_bucket_name" {
     type = string
-    default = "deploy-aws-lambda-image-tf-state"
 }
 
 variable "backend_bucket_key_name" {
     type = string
-    default = "deploy_lambda.tfstate"
 }
